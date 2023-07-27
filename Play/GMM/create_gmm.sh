23b878b7df4f4c3b09b50dd7c4eeb64b03dbbc2a
Kenobi98 <majilakartik123@gmail.com>
2023-07-27 21:30:37 +0530

    Minor changes

--- Play/GMM/create_gmm.sh
+++ Play/GMM/create_gmm.sh
@@ -6,10 +6,10 @@
 mapname=$1
 threshold=$2
 
-for i in 5 10 20 30 40  #number of Gaussians
+for i in 5 10 20  #number of Gaussians
 do
 
-~/imp-clean/build/setup_environment.sh python ~/imp-clean/imp/modules/isd/pyext/src/create_gmm.py $mapname $i $mapname.gmm.$i.txt -s $threshold -m $mapname.gmm.$i.mrc -a 2.0 -i 10000000
+python ~/imp-clean/imp/modules/isd/pyext/src/create_gmm.py $mapname $i $mapname.gmm.$i.txt -s $threshold -m $mapname.gmm.$i.mrc -a 2.0 -i 10000000
 
 done
 
