fd65187ac539929e636fa98f8b3752ba4d6d96d2
Shruthi Viswanath <shruthivis@users.noreply.github.com>
2024-06-25 20:29:22 +0530

    Update create_gmm.sh

--- Play/GMM/create_gmm.sh
+++ Play/GMM/create_gmm.sh
@@ -9,7 +9,7 @@ threshold=$2
 for i in 5 20 25 30 50 #number of Gaussians
 do
 
-python /usr/lib/python3.6/dist-packages/IMP/isd/create_gmm.py $mapname $i $mapname.gmm.$i.txt -s $threshold -m $mapname.gmm.$i.mrc -a 2.0 -i 10000000
+python /apps/conda/lib/python3.11/site-packages/IMP/isd/create_gmm.py $mapname $i $mapname.gmm.$i.txt -s $threshold -m $mapname.gmm.$i.mrc -a 2.0 -i 10000000
 
 done
 
